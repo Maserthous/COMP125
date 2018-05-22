2cc1b584eef6840e24ff5fdce39215ba69283984
Maserthous <35930761+Maserthous@users.noreply.github.com>
2018-05-22 18:05:41 -0400

    Assignment 1

--- COMP125-001/Assignment1/Assignment1.js
+++ COMP125-001/Assignment1/Assignment1.js
@@ -10,9 +10,35 @@ function calcCelsius() {
     result = (tempFar - 32) * 5 / 9;
     document.getElementById("display").innerHTML = Math.round(result);
 }
+function calcBMR() {
+    var bmr = 0;
+    var cal = 0;
+    var tempSex = document.getElementById("selSex").value;
+    var tempAge = document.getElementById("txtAge").value;
+    var tempHeight = document.getElementById("txtHeight").value;
+    var tempWeight = document.getElementById("txtWeight").value;
+    var tempAct = document.getElementById("radAct").value;
+    if (tempSex == "M") { // Male BMR = (10 × weight in kg) + (6.25 × height in cm) - (5 × age in years) + 5
+        bmr = (10 * tempWeight) + (6.25 * tempHeight) - (5 * tempAge) + 5;
+    }
+    if (tempSex == "F") { // BMR = (10 × weight in kg) + (6.25 × height in cm) - (5 × age in years) - 161
+        bmr = (10 * tempWeight) + (6.25 * tempHeight) - (5 * tempAge) - 161;
+    }
+    if (tempAct == "light") {
+        cal = bmr * 1.53;
+    }
+    if (tempAct == "moderate") {
+        cal = bmr * 1.76;
+    }
+    if (tempAct == "heavy") {
+        cal = bmr * 2.25;
+    }
+    document.getElementById("bmr").innerHTML = bmr;
+    document.getElementById("calories").innerHTML = Math.round(cal);
+}
 function clearField() {
     //clear Far text box
     document.getElementById("txtFar").value = "";
 }
-document.getElementById("btnCalculate").addEventListener("click", calcCelsius, false);
+document.getElementById("btnCalculate").addEventListener("click", calcBMR, false);
 document.getElementById("txtFar").addEventListener("click", clearField, false);
